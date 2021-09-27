e87aa124a70a48ce8d3e507b898a5d5cade7a683
Adel Pakrad <adelpakrad@gmail.com>
2021-09-27 10:43:39 -0600

    parallax VH adjustment 3

--- src/Components/home/Paralax.js
+++ src/Components/home/Paralax.js
@@ -13,6 +13,12 @@ import Rellax from "rellax";
 
 export default function Landing() {
 
+    const [scrolling, setScrolling] = useState("Flex")
+
+   useEffect(() => {
+    console.log(window.pageYOffset)
+   })
+
     useEffect(() => {
         new Rellax(".fourth", { // <---- Via class name
           speed: -5,
@@ -53,44 +59,39 @@ export default function Landing() {
 
     return (
         <Container>
-            <Div>
+            <SvgBoxLrgScreen>
                 <Img src={Fourth} className="fourth" alt="layer" />
                 <Img src={Third} className="third" alt="layer" />
                 <Img src={Second} className="second" alt="layer" />
                 <Img src={First} className="first" alt="layer" />
-            </Div>
-            <DivSm>
+            </SvgBoxLrgScreen>
+            <SvgBoxSmlScreen>
                 <Img src={FourthSm} className="fourth" alt="layer" />
                 <Img src={ThirdSm} className="third" alt="layer" />
                 <Img src={SecondSm} className="second" alt="layer" />
                 <Img src={FirstSm} className="first" alt="layer" />
-            </DivSm>
-            <Div2>
-                <H1 className="fourth">"THE EARTH IS ART, THE PHOTOGRAPHER IS ONLY A WITNESS." <Div3>Thomas Merton</Div3>
-                    <Div4><P><I></I><I></I><I></I></P></Div4>
-                </H1>
-            </Div2>
+            </SvgBoxSmlScreen>
+            <TextBox>
+                <Qoute className="fourth">"THE EARTH IS ART, THE PHOTOGRAPHER IS ONLY A WITNESS." <NameBox>Thomas Merton</NameBox>
+                    <ArrowBox onScroll={() => hideArrow()}><P><ArrowDown></ArrowDown><ArrowDown></ArrowDown><ArrowDown></ArrowDown></P></ArrowBox>
+                </Qoute>
+            </TextBox>
         </Container>
     )
 }
 
 const Container = styled.div`
     margin: 0;
-    /* display: flex; */
-    /* height: 100%; */
-    /* width: 100%; */
-    /* position: relative ; */
-    /* z-index: 1; */
 `
 
-const Div = styled.div`
+const SvgBoxLrgScreen = styled.div`
     display: flex;
     width: 100%;
     @media (max-width: 600px) {
         display: none;
     }
 `
-const DivSm = styled.div`
+const SvgBoxSmlScreen = styled.div`
     display: none;
     @media (max-width: 600px) {
         width: 100%;
@@ -105,7 +106,7 @@ const Img = styled.img`
     width: 100%;
 `
 
-const Div2 = styled.div`
+const TextBox = styled.div`
     display: flex;
     flex-direction: row;
     width: 80%;
@@ -113,16 +114,27 @@ const Div2 = styled.div`
     position: absolute;
     margin: 10%;
     @media only screen and (max-width: 600px) {
-        margin-top: 25%
+        margin-top: 25%;
+        justify-content: center;
     }
 `
-const Div3 = styled.div`
+const NameBox = styled.div`
     display: block;
     color: lightpink;
     padding-top: 5%;
     opacity: .8;
+    font-size: 1rem;
+    @media only screen and (max-width: 900px) {
+        font-size: .8rem;
+    } 
+    @media only screen and (max-width: 600px) {
+        display: flex;
+        justify-content: center;
+        font-size: .6rem;
+    } 
 `
-const Div4 = styled.div`
+const ArrowBox = styled.div`
+    /* display: ${scrolling}; */
     margin: 0 auto;
     width: 18px;
     color: lightpink;
@@ -144,8 +156,7 @@ const Div4 = styled.div`
     }
 `
 
-const H1 = styled.h1`
-    font-size: 1.3rem;
+const Qoute = styled.h1`
     color: white;
     margin: 5%;
     width: 250px;
@@ -153,32 +164,24 @@ const H1 = styled.h1`
     font-size: 1.1rem;
     line-height:1.5rem;
     opacity: .8;
-    @media only screen and (max-width: 600px) {
-        display: none;
-        font-size: 1.3rem;
-        color: white;
-        /* margin: 5%; */
+    text-align: center;
+    @media only screen and (max-width: 900px) {
         width: 150px;
+        font-size: .8rem;
+        line-height:1.2rem;
+    } 
+    @media only screen and (max-width: 600px) {
         font-size: .8rem;
         line-height:1.2rem;
     } 
 `
 
-const H2 = styled.h4`
-    display: flex;
-    flex-direction: column;
-`
-
-const Span = styled.span`
-    display: flex;
-    font-size: .8rem;
-`
 const P = styled.p`
     color: white;
     display: flex;
     flex-direction: column;
 `
-const I = styled.i`
+const ArrowDown = styled.i`
     margin: 2px;
     color: white;
     transform: rotate(45deg);
