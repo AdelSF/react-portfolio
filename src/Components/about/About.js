1f27e9b2f7052a547ecfaa361cd2ed9e313ded4e
Paakraad <paakraad@Adels-MBP-2.attlocal.net>
2023-03-07 19:05:04 -0800

    changing the about to  static

--- src/Components/about/About.js
+++ src/Components/about/About.js
@@ -34,11 +34,32 @@ export default function About(){
   
     return (
         <Container>
-            {data.data.map(artist => (
-                // console.log(artist.attributes.title)
-                // <div key={artist.id}>
-                //     <h1>{artist.attributes.title}</h1>
-                // </div>
+            <Div>
+                <Div1>
+                    <H1>Director</H1>
+                    <P>My name is Adel Paakraad. I have always considered myself a lifelong student of the arts. I had the good fortune growing up in a family surrounded by artists; that's why I started painting and drawing at a young age. But since I started film school in 2011, I truly fell in love with photography, which was the beginning of a new journey.</P>
+                    <P>Since beginning photography, my passion has been to capture the beauty and the simplicity in people's lives. As I learn more about them, I use their stories as an inspiration to create art.</P> 
+                </Div1>
+                <Img src={"https://icecube-us-303.icedrive.io/thumbnail?p=mxbPN8r2D0yQuC%2BLkr%2BBgz1cTZ8nngrkzL9J7XLlOhuDNghttX3Z9NVbOtE7H7dq0GmF8Hj9zOrdSgqHojK5zednNOK47f%2BBQ%2BGhLTYR%2FtBSvOWpFN%2BPt6xYs5qeSO5t&w=1280&h=1280&m=cropped"} alt="Team member" />
+            </Div>
+            
+            <Div>
+                <Div1>
+                    <H1>Technical Advisor</H1>
+                    <P>Payam Jafari is a screenwriter, director, and painter based in Tehran. He got his Master's in motion picture and television directing at the Academy of Art University in San Francisco in 2017 and Sooreh Art University in Tehran. Since his graduation, he has worked with leading actors, producers, and artists in the United States and Iran.</P> 
+                </Div1>
+                <Img src={"https://icecube-us-303.icedrive.io/thumbnail?p=Eunzkuqy9wwyDuteHmDh5AakDIGnIQeOV3o3ztqPDIZXyiPFB1HaWDEytss%2FEBd6De7FJeedqccy2rFETDRp1MZ1UdBlMhdt5A%2Fzu4JqxbFv3Bm1Wcz2QhLQF5Z81Wf0&w=1280&h=1280&m=cropped"} alt="Team member" />
+            </Div>
+
+            <Div>
+                <Div1>
+                    <H1>Illustrator - Editor</H1>
+                        <P>Neda Lorestani is a graphic designer, painter, and photo editor based in Tehran. She got her Bachelor's in Art University in Kermanshah in 2016 and has had over a decade of experience in painting and drawing. Since her graduation, she has been working with various companies on various projects.</P> 
+                </Div1>
+                <Img src={"https://icecube-us-303.icedrive.io/thumbnail?p=PpHEPu41jPPsfVcMsSe3HGfjpXVjbPPTlF%2FKEwpi0IGITPjCidxJ4dm6pOthF%2B3JCSXufgparUas3vuuYpYDryHsQ2EKYk06uRlIlUWvY%2B%2BM3r%2F7AL91cMOnkp4LeoX9&w=1280&h=1280&m=cropped"} alt="Team member" />
+            </Div>
+
+            {/* {data.data.map(artist => (
                 <Div key={artist.id}>
                     <Div1>
                         <H1>{artist.attributes.title}</H1>
@@ -46,10 +67,7 @@ export default function About(){
                     </Div1>
                     <Img src={artist.attributes.imgUrl} alt="Team member" />
                 </Div>
-            
-            
-                
-            ))}
+            ))} */}
         </Container>
     )
 }
@@ -99,28 +117,3 @@ const Img = styled.img`
     }
 `
 
-
-            {/* <Div>
-                <Div1>
-                    <H1>Director</H1>
-                    <P>My name is Adel Paakraad. I have always considered myself a lifelong student of the arts. I had the good fortune growing up in a family surrounded by artists; that's why I started painting and drawing at a young age. But since I started film school in 2011, I truly fell in love with photography, which was the beginning of a new journey.</P>
-                    <P>Since beginning photography, my passion has been to capture the beauty and the simplicity in people's lives. As I learn more about them, I use their stories as an inspiration to create art.</P> 
-                </Div1>
-                <Img src={"https://icecube-us-303.icedrive.io/thumbnail?p=mxbPN8r2D0yQuC%2BLkr%2BBgz1cTZ8nngrkzL9J7XLlOhuDNghttX3Z9NVbOtE7H7dq0GmF8Hj9zOrdSgqHojK5zednNOK47f%2BBQ%2BGhLTYR%2FtBSvOWpFN%2BPt6xYs5qeSO5t&w=1280&h=1280&m=cropped"} alt="Team member" />
-            </Div>
-            
-            <Div>
-                <Div1>
-                    <H1>Technical Advisor</H1>
-                    <P>Payam Jafari is a screenwriter, director, and painter based in Tehran. He got his Master's in motion picture and television directing at the Academy of Art University in San Francisco in 2017 and Sooreh Art University in Tehran. Since his graduation, he has worked with leading actors, producers, and artists in the United States and Iran.</P> 
-                </Div1>
-                <Img src={"https://icecube-us-303.icedrive.io/thumbnail?p=Eunzkuqy9wwyDuteHmDh5AakDIGnIQeOV3o3ztqPDIZXyiPFB1HaWDEytss%2FEBd6De7FJeedqccy2rFETDRp1MZ1UdBlMhdt5A%2Fzu4JqxbFv3Bm1Wcz2QhLQF5Z81Wf0&w=1280&h=1280&m=cropped"} alt="Team member" />
-            </Div>
-
-            <Div>
-                <Div1>
-                    <H1>Illustrator - Editor</H1>
-                        <P>Neda Lorestani is a graphic designer, painter, and photo editor based in Tehran. She got her Bachelor's in Art University in Kermanshah in 2016 and has had over a decade of experience in painting and drawing. Since her graduation, she has been working with various companies on various projects.</P> 
-                </Div1>
-                <Img src={"https://icecube-us-303.icedrive.io/thumbnail?p=PpHEPu41jPPsfVcMsSe3HGfjpXVjbPPTlF%2FKEwpi0IGITPjCidxJ4dm6pOthF%2B3JCSXufgparUas3vuuYpYDryHsQ2EKYk06uRlIlUWvY%2B%2BM3r%2F7AL91cMOnkp4LeoX9&w=1280&h=1280&m=cropped"} alt="Team member" />
-            </Div> */}
